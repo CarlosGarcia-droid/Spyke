c11324c7104cc41fb4c05240e388f770add3973c
carlos <al376028@uji.es>
2020-12-05 18:01:32 +0100

    Parte del backend terminada, creamos peticiones y las listamos

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const api = express.Router();
+const User = require('../models/Peticiones');
 
-api.get('/users', (req, res) => {
-    res.json('Hola loko');
+const faker = require('faker');
+
+
+api.get('/listarPeticiones', async (req, res) => {
+    const users = await( User.find() );
+    res.json(users);
+});
+
+api.get('/crearPeticiones', async (req, res) => {
+
+    
+    for(let i = 0; i < 5; i++){
+        await(User.create({
+            idPeticion: faker.random.alphaNumeric(),
+            nombre: faker.name.jobTitle(),
+            descripcion: faker.name.jobDescriptor()
+        }));
+    }
+
+    res.json({message: '5 peticiones creadas'});
+
 });
 
 module.exports = api; 
