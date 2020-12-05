9e60a4f77da54eda4744f19038673ee1ac431568
carlos <al376028@uji.es>
2020-12-05 13:50:53 +0100

    Preparacion de mongodb para el spike

---
+++ Routes/users.js
@@ -0,0 +1,7 @@
+const{ Router } = require('express');
+
+const router = Router();
+
+router.get('/api/users', (req, res) => {
+    res.json('User list');
+});
---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+async function main(){
+    await app.listen(4000);
+    console.log('Server on por 4000');
+}
+
+
+main();
+// Flutter --> 36:09
