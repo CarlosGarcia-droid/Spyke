63e017c2364895ad8dfafe053571268d7928c927
carlos <al376028@uji.es>
2020-12-07 12:54:11 +0100

    Correcciones

--- src/routes/peticiones.js
+++ src/routes/peticiones.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const api = express.Router();
-const User = require('../models/Peticiones');
+const Peticion = require('../models/Peticiones');
 
 const faker = require('faker');
 
 
 api.get('/listarPeticiones', async (req, res) => {
-    const users = await( User.find() );
-    res.json(users);
+    const peticiones = await( Peticion.find() );
+    res.json({peticiones});
 });
 
 api.get('/crearPeticiones', async (req, res) => {
 
     
     for(let i = 0; i < 5; i++){
-        await(User.create({
+        await(Peticion.create({
             idPeticion: faker.random.alphaNumeric(),
             nombre: faker.name.jobTitle(),
             descripcion: faker.name.jobDescriptor()
