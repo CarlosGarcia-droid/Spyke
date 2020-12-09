99fed76cb51ae1048e094d3a6c3d83704963347a
carlos <al376028@uji.es>
2020-12-10 00:53:02 +0100

    intento de deploy

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const app = require('./app');
 const { connect } = require('./database')
 
+let port = process.env.PORT || 4000;
+
 async function main(){
     await connect();
     console.log('Conectado a la base de datos');
 
-    app.listen(4000);
+    app.listen(port);
     console.log('Server on port 4000');
 }
 
